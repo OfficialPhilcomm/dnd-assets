ff33d076a09d9216d453e026c2ae59905042ab27
Philipp Schlesinger <info@philcomm.dev>
2020-09-19 10:04:46 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import IndexMain from "./components/index_main/IndexMain";
 import Nav from "./components/nav/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <IndexMain />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={IndexMain} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
