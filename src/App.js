cb523dc0eab9d32356bca6f4c67631e5e1727c0a
Philipp Schlesinger <info@philcomm.dev>
2020-09-19 09:52:07 +0200

    Add index main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import IndexMain from "./components/index_main/IndexMain";
 import Nav from "./components/nav/Nav";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <IndexMain />
     </div>
   );
 }
---
+++ src/components/index_main/IndexMain.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./index-main.css";
+
+export default class IndexMain extends Component {
+  render() {
+    return <main>This is the main</main>;
+  }
+}
