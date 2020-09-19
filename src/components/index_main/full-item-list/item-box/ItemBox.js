0b84566593f488d10d8f57ffd863c7d089871f51
Philipp Schlesinger <info@philcomm.dev>
2020-09-19 10:38:25 +0200

    Finalize item list styling

--- src/components/index_main/full-item-list/item-box/ItemBox.js
+++ src/components/index_main/full-item-list/item-box/ItemBox.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 import "./item-box.css";
+import QuestionMark from "../../../../img/question-circle-regular.svg";
 
 export default class ItemBox extends Component {
   render() {
-    return <div className="fullItemList-itemBox">This is it</div>;
+    const item = this.props.item || {};
+
+    const itemSpriteUrl = item.spriteUrl || QuestionMark;
+    const itemName = item.name || "Name missing";
+
+    return (
+      <div className="fullItemList-itemBox">
+        <img className="fullItemList-itemBox-image" src={itemSpriteUrl} />
+        <div className="fullItemList-itemBox-name">{itemName}</div>
+      </div>
+    );
   }
 }
