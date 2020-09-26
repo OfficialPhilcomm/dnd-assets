57ff353f11dde53b5164494623648d37feb2380c
Philipp Schlesinger <info@philcomm.dev>
2020-09-26 12:13:20 +0200

    Add more item data

--- src/StringUtils.js
+++ src/StringUtils.js
@@ -2,37 +2,18 @@ export default class StringUtils {
   static items = [
     {
       id: "6f1ac1a1-b4d7-44d3-b12f-f4955dfed557",
-      name: "The Sword!",
+      name: "Galadriel’s Edge",
+      description:
+        "Long ago the great Elven king Galadriel travelled into a dark land of shadow and evil. Legends say Galadriel vanished, perhaps falling in battle or captured by his enemy, non can say for sure. What is known however is the a year after Galadriel entered the shadowed lands his horse returned home, carrying on his sword and scabbard.",
       playerLevel: 1,
       rarity: 1,
       racialOrigin: "human",
       unique: false,
       itemType: "sword",
       cursed: false,
-    },
-    {
-      id: "3c7efe5d-e23b-43e4-a40d-f0f818adf95f",
-      name: "mystic spear",
-      spriteUrl:
-        "https://image.shutterstock.com/image-vector/scratchy-ink-drawing-spear-260nw-46828840.jpg",
-      playerLevel: 1,
-      rarity: 1,
-      racialOrigin: "dwarfes",
-      unique: false,
-      itemType: "spear",
-      cursed: false,
-    },
-    {
-      id: "1593e239-2db0-4f99-b3c3-d8fe4767658d",
-      name: "some sword",
-      spriteUrl:
-        "https://image.shutterstock.com/image-vector/sword-hand-drawn-sketch-vector-260nw-480665485.jpg",
-      playerLevel: 2,
-      rarity: 2,
-      racialOrigin: "evlish",
-      unique: true,
-      itemType: "short_sword",
-      cursed: false,
+      damage: "1D8+1 Slashing",
+      weight: 1.5,
+      properties: "versatile (1D10)",
     },
   ];
 
