c738d1fdf07d59c33c2fceded167faff4fc0fdb4
Philipp Schlesinger <info@philcomm.dev>
2020-11-29 14:41:15 +0100

    Make details table look better

--- src/components/item-details/details-table/DetailsTable.js
+++ src/components/item-details/details-table/DetailsTable.js
@@ -6,37 +6,63 @@ export default class DetailsTable extends Component {
   render() {
     const item = this.props.item;
 
+    const playerLevel =
+      item.playerLevel === 0
+        ? "Low level"
+        : item.playerLevel === 1
+        ? "Mid level"
+        : item.playerLevel === 2
+        ? "High level"
+        : "Unknown level";
+
+    const rarity =
+      item.rarity === 0
+        ? "Common"
+        : item.rarity === 1
+        ? "Uncommon"
+        : item.rarity === 2
+        ? "Rare"
+        : "Unknown rarity";
+
     return (
       <table className="detailsTable">
         <tbody className="detailsTable-tbody">
           <tr className="detailsTable-row">
             <td className="detailsTable-row-title">Player Level</td>
-            <td className="detailsTable-row-content">{item.playerLevel}</td>
+            <td className="detailsTable-row-content">{playerLevel}</td>
           </tr>
 
           <tr className="detailsTable-row">
             <td className="detailsTable-row-title">Rarity</td>
-            <td className="detailsTable-row-content">{item.rarity}</td>
+            <td className="detailsTable-row-content">{rarity}</td>
           </tr>
 
           <tr className="detailsTable-row">
             <td className="detailsTable-row-title">Racial Origin</td>
-            <td className="detailsTable-row-content">{item.racialOrigin}</td>
+            <td className="detailsTable-row-content">
+              {StringUtils.humanize(item.racialOrigin)}
+            </td>
           </tr>
 
           <tr className="detailsTable-row">
             <td className="detailsTable-row-title">Unique</td>
-            <td className="detailsTable-row-content">{item.unique}</td>
+            <td className="detailsTable-row-content">
+              {item.unique ? "Yes" : "No"}
+            </td>
           </tr>
 
           <tr className="detailsTable-row">
             <td className="detailsTable-row-title">Item Type</td>
-            <td className="detailsTable-row-content">{item.itemType}</td>
+            <td className="detailsTable-row-content">
+              {StringUtils.humanize(item.itemType)}
+            </td>
           </tr>
 
           <tr className="detailsTable-row">
             <td className="detailsTable-row-title">Cursed</td>
-            <td className="detailsTable-row-content">{item.cursed}</td>
+            <td className="detailsTable-row-content">
+              {item.cursed ? "Yes" : "No"}
+            </td>
           </tr>
 
           {item.damage ? (
@@ -100,7 +126,9 @@ export default class DetailsTable extends Component {
           {item.elementalType ? (
             <tr className="detailsTable-row">
               <td className="detailsTable-row-title">Elemental Type</td>
-              <td className="detailsTable-row-content">{item.elementalType}</td>
+              <td className="detailsTable-row-content">
+                {StringUtils.humanize(item.elementalType)}
+              </td>
             </tr>
           ) : null}
         </tbody>
