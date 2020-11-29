2cbcde78ede6a215cacfcd2f1f0dac2ec4391236
Philipp Schlesinger <info@philcomm.dev>
2020-11-29 14:06:23 +0100

    Add elven racial origin

--- src/ItemController.test.js
+++ src/ItemController.test.js
@@ -49,7 +49,7 @@ describe("check for correct item structure", () => {
   ];
 
   const idRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
-  const racialOrigins = ["human", "fiendish"];
+  const racialOrigins = ["human", "fiendish", "elven"];
   const itemTypes = ["sword", "helmet"];
   const elementalTypes = ["normal", "fire", "ice"];
 
