8dea73b2c0b31905874d7baa4ed7350a4c061666
Philipp Schlesinger <info@philcomm.dev>
2020-11-29 14:01:01 +0100

    Extend tests to include all possible item keys

--- src/ItemController.test.js
+++ src/ItemController.test.js
@@ -27,9 +27,37 @@ describe("check if ids have same prefix", () => {
 describe("check for correct item structure", () => {
   const items = ItemController.items;
 
+  const itemAttributes = [
+    "id",
+    "name",
+    "description",
+    "playerLevel",
+    "rarity",
+    "racialOrigin",
+    "unique",
+    "itemType",
+    "cursed",
+    "damage",
+    "weight",
+    "properties",
+    "customProperties",
+    "armorClass",
+    "hitPoints",
+    "speed",
+    "skills",
+    "elementalType",
+  ];
+
+  const idRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+  const racialOrigins = ["human", "fiendish"];
+  const itemTypes = ["sword", "helmet"];
+  const elementalTypes = ["normal", "fire", "ice"];
+
   items.forEach((item) => {
     test(`in item ${item.name || "name missing"}`, () => {
-      const idRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+      Object.keys(item).forEach((key) => {
+        expect(itemAttributes).toContain(key);
+      });
 
       expect(item.id).toBeDefined();
       expect(typeof item.id).toEqual("string");
@@ -43,27 +71,72 @@ describe("check for correct item structure", () => {
 
       expect(item.playerLevel).toBeDefined();
       expect(typeof item.playerLevel).toEqual("number");
+      expect(item.playerLevel).toBeGreaterThanOrEqual(0);
+      expect(item.playerLevel).toBeLessThanOrEqual(2);
 
       expect(item.rarity).toBeDefined();
       expect(typeof item.rarity).toEqual("number");
+      expect(item.rarity).toBeGreaterThanOrEqual(0);
+      expect(item.rarity).toBeLessThanOrEqual(2);
 
       expect(item.racialOrigin).toBeDefined();
       expect(typeof item.racialOrigin).toEqual("string");
+      expect(racialOrigins).toContain(item.racialOrigin);
 
       expect(item.unique).toBeDefined();
       expect(typeof item.unique).toEqual("boolean");
 
       expect(item.itemType).toBeDefined();
       expect(typeof item.itemType).toEqual("string");
+      expect(itemTypes).toContain(item.itemType);
 
       expect(item.cursed).toBeDefined();
-      expect(typeof item.racialOrigin).toEqual("string");
+      expect(typeof item.cursed).toEqual("boolean");
+
+      if (item.damage) {
+        expect(typeof item.damage).toEqual("string");
+      }
 
       expect(item.weight).toBeDefined();
       expect(typeof item.weight).toEqual("number");
 
+      if (item.properties) {
+        expect(typeof item.properties).toEqual("string");
+      }
+
+      if (item.customProperties) {
+        item.customProperties.forEach((customProperty) => {
+          expect(customProperty.name).toBeDefined();
+          expect(typeof customProperty.name).toEqual("string");
+
+          expect(customProperty.text).toBeDefined();
+          expect(typeof customProperty.text).toEqual("string");
+
+          expect(Object.keys(customProperty).length).toEqual(2);
+        });
+      }
+
+      if (item.armorClass) {
+        expect(typeof item.armorClass).toEqual("number");
+        expect(item.armorClass).toBeGreaterThanOrEqual(1);
+      }
+
+      if (item.hitPoints) {
+        expect(typeof item.hitPoints).toEqual("number");
+        expect(item.hitPoints).toBeGreaterThanOrEqual(1);
+      }
+
+      if (item.speed) {
+        expect(typeof item.speed).toEqual("string");
+      }
+
+      if (item.skills) {
+        expect(typeof item.skills).toEqual("string");
+      }
+
       expect(item.elementalType).toBeDefined();
       expect(typeof item.elementalType).toEqual("string");
+      expect(elementalTypes).toContain(item.elementalType);
     });
   });
 });
