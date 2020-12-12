3e3affef57c8e754f134561d4a59df5d141566a3
Philipp Schlesinger <info@philcomm.dev>
2020-12-12 11:29:52 +0100

    Add creator controller tests

---
+++ src/creators/CreatorController.test.js
@@ -0,0 +1,32 @@
+import CreatorController from "./CreatorController";
+import CreatorData from "./creators.json";
+
+test("check for creator name uniqueness", () => {
+  const creatorNames = CreatorController.creators.map(
+    (creator) => creator.name
+  );
+  const uniqueNames = new Set(creatorNames);
+
+  expect(creatorNames.length).toEqual(uniqueNames.size);
+});
+
+describe("check if creator has required tags", () => {
+  const creators = CreatorController.creators.forEach((creator) => {
+    test(`for creator ${creator.name}`, () => {
+      expect(creator.name).toBeDefined();
+      expect(creator.socialMedia).toBeDefined();
+    });
+  });
+});
+
+describe("check if creators has supported social media", () => {
+  const supportedSocialMedia = ["instagram", "twitter"];
+
+  const creators = CreatorController.creators.forEach((creator) => {
+    test(`for creator ${creator.name}`, () => {
+      creator.socialMedia.forEach((socialMedia) => {
+        expect(supportedSocialMedia).toContain(socialMedia.platform);
+      });
+    });
+  });
+});
